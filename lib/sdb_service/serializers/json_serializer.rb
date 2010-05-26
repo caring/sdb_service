72014b8768c4a6d99730dc27cb3b6d89fe771af3
Chris Eppstein <chris@eppsteins.net>
2010-05-26 12:40:03 -0700

    json serialization is smart enough to not dupe the parsing of an array, since simpledb assumes the base datatype at the top level is an array in the first place.

--- lib/sdb_service/serializers/json_serializer.rb
+++ lib/sdb_service/serializers/json_serializer.rb
@@ -5,7 +5,7 @@ module SdbService
   class JsonSerializer < Serializer
   
     def serialize_payload(payload)
-      payload.is_a?(String) ? payload : JSON.generate(payload)
+      is_serializable?(payload) ? JSON.generate(payload) : payload 
     end
   
     def deserialize_payload(payload)
@@ -13,6 +13,16 @@ module SdbService
         return JSON.parse(payload)
       rescue JSON::ParserError
         return payload
+      end
+    end
+    
+    private
+    
+    def is_serializable?(payload)
+      if payload.is_a?(String) || (payload.is_a?(Array) && !payload.find { |p| p.class != String })
+        false
+      else
+        true
       end
     end
   
